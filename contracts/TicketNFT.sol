d30810f4bb03259726ced7a832eec466a440699b
leonardokhorliey <ebubeokoli@rocketmail.com>
2022-10-27 17:14:19 +0100

    Project setup

---
+++ contracts/TicketNFT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+
+contract TicketNFT is ERC721 {
+
+
+    constructor(name, symbol) {
+        ERC721(name, symbol);
+
+
+    }
+
+    
+}
